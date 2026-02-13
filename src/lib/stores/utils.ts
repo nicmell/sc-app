b489248a2a3c1de6141d51c32dea9aaa5137f9df
nicolamelloni <nicola.melloni@slesh.it>
2026-02-13 18:08:03 +0100

    wip

--- src/lib/stores/utils.ts
+++ src/lib/stores/utils.ts
@@ -1,14 +1,21 @@
 export type Action<T extends string = string, P = void> =
   [P] extends [void] ? { type: T } : { type: T; payload: P };
 
+export type Actions<T extends keyof any = string> = Record<T, Action>;
+
 export type ActionCreator<T extends string = string, P = void> =
   [P] extends [void]
     ? { (): Action<T>; type: T }
     : { (payload: P): Action<T, P>; type: T };
 
-type SliceReducer<S> = (state: S, action: Action) => void;
+export type CaseReducer<S, A extends Action = Action> = (state: S, action: A) => void;
 
-type CaseReducers<S> = Record<string, (state: S, ...args: any[]) => void>;
+export type CaseReducers<S, A = Record<string, any>> = {
+  [K in keyof A]: CaseReducer<S, A[K] & Action>
+};
+
+export type InferAction<T extends { actions: Record<string, (...args: any[]) => any> }> =
+  ReturnType<T["actions"][keyof T["actions"]]>;
 
 type InferActionCreators<Name extends string, R> = {
   [K in keyof R & string]: R[K] extends (...args: infer A) => any
@@ -17,9 +24,6 @@ type InferActionCreators<Name extends string, R> = {
       : ActionCreator<`${Name}/${K}`>
     : never;
 };
-
-export type InferAction<T extends { actions: Record<string, (...args: any[]) => any> }> =
-  ReturnType<T["actions"][keyof T["actions"]]>;
 
 export function createAction<T extends string>(
   type: T,
@@ -36,14 +40,14 @@ export function createAction(type: string, prepare?: (...args: any[]) => any) {
   );
 }
 
-export function createReducer<S, A extends Action>(
+export function createReducer<S, R extends CaseReducers<S>>(
   initialState: S,
-  handlers: { [T in A["type"]]?: (state: S, action: Extract<A, { type: T }>) => void },
+  reducers: R,
 ) {
-  const reducer: SliceReducer<S> = (state, action) => {
-    const handler = handlers[action.type as A["type"]];
+  const reducer: CaseReducer<S> = (state, action) => {
+    const handler = reducers[action.type];
     if (handler) {
-      (handler as SliceReducer<S>)(state, action);
+      handler(state, action);
     }
   };
   return {initialState, reducer};
@@ -55,27 +59,27 @@ export function createSlice<S, Name extends string, R extends CaseReducers<S>>(c
   reducers: R;
 }): {
   initialState: S;
-  reducer: SliceReducer<S>;
+  reducer: CaseReducer<S>;
   actions: InferActionCreators<Name, R>;
 } {
   const {name, initialState, reducers} = config;
   const actions = {} as Record<string, any>;
-  const handlerMap = {} as Record<string, (state: S, action: Action) => void>;
+  const handlerMap = {} as CaseReducers<S>;
 
   for (const key of Object.keys(reducers)) {
     const type = `${name}/${key}`;
     actions[key] = createAction(type, (payload: any) => payload);
-    handlerMap[type] = reducers[key] as (state: S, action: Action) => void;
+    handlerMap[type] = reducers[key] as CaseReducer<S>;
   }
 
-  const {reducer} = createReducer<S, Action>(initialState, handlerMap);
+  const {reducer} = createReducer(initialState, handlerMap);
 
   return {initialState, reducer, actions: actions as any};
 }
 
 export function combineReducers<S extends object>(
-  reducers: { [K in keyof S]: SliceReducer<S[K]> },
-): SliceReducer<S> {
+  reducers: { [K in keyof S]: CaseReducer<S[K]> },
+): CaseReducer<S> {
   const keys = Object.keys(reducers) as (keyof S)[];
   return (state, action) => {
     for (const key of keys) {
